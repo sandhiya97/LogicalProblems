d7a89cdae299edde27f80e0521b46d1c96a56c55
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-21 08:17:31 +0530

    StopWatch program using Java

--- src/LogicalProblems/LogicalProblems.java
+++ src/LogicalProblems/LogicalProblems.java
@@ -1,21 +1,52 @@
 package LogicalProblems;
 import java.util.Scanner;
+
+import StopWatch.StopWatch;
 public class LogicalProblems {
 
-    public static void main(String args[]) {
-    	char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
-		int max=100000000;
-		int random=(int) (Math.random()*max);	
-		StringBuffer sb=new StringBuffer();
+   
+	public long startTimer=0;
+	public long stopTimer=0;
+	public long elapsed;
+	
+	//to start timer
+	public void start()
+	{
+		startTimer=System.currentTimeMillis();
+		System.out.println("Start Time is: "+startTimer);
+	}
+	
+	// to stop timer
+	public void stop()
+	{
+		stopTimer=System.currentTimeMillis();
+		System.out.println("Stop Time is: "+stopTimer);
+	}
+	
+	public long getElapsedTime()
+	{
+		elapsed=stopTimer-startTimer;
+		return elapsed;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		StopWatch sw=new StopWatch();
 		
-		while (random>0)
-		{
-			sb.append(chars[random % chars.length]);
-			random /= chars.length;
-		}
+		System.out.println("Press '1' to Start Time: ");
+		u.inputInteger();
+		sw.start();
 
-		String couponCode=sb.toString();
-		System.out.println("Coupon Code: "+couponCode);	
+		System.out.println();
+		System.out.println("Press '2' to Stop Time: ");
+		u.inputInteger();
+		sw.stop();
+
+		long l=sw.getElapsedTime();
+		System.out.println();
+		System.out.println("Total Time Elapsed(in millisec) is:"+l);
+		System.out.println();
+		System.out.println("Converting millisec to seconds: "+(l/1000)+" sec");
         
     }
 }
