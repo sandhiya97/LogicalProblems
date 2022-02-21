54b36dc84eff57c23d5c909216f7cf3684e4daea
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-21 07:54:48 +0530

    Prime Number Program using Java

--- src/LogicalProblems/LogicalProblems.java
+++ src/LogicalProblems/LogicalProblems.java
@@ -1,37 +1,27 @@
 package LogicalProblems;
-import java.util.Scanner;  
-public class LogicalProblems
-{  
-//function that checks if the given number is perfect or not  
-static long isPerfect(long num)  
-{  
-//variable stores the sum      
-long sum=0;  
-//executes until the condition becomes false  
-for(int i=1; i <= num/2; i++)  
-{  
-if(num % i == 0)  
-{  
-//calculates the sum of factors  
-sum=sum + i;  
-} //end of if  
-}  //end of for  
-//returns the sum of factors  
-return sum;   
-} //end of method  
-public static void main(String args[])    
-{  
-long number, s;  
-Scanner sc=new Scanner(System.in);         
-System.out.print("Enter the number: ");  
-//reads a number from the user  
-number=sc.nextLong();  
-//calling the function  
-s = isPerfect(number);  
-//compares sum with the number  
-if(s==number)  
-System.out.println(number+" is a perfect number");  
-else  
-System.out.println(number+" is not a perfect number");   
-}   
-}  
+import java.util.Scanner;
+public class LogicalProblems {
+
+    public static void main(String args[]) {
+        long number, s;
+        int i, n, m = 0, flag = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n = sc.nextInt();
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            System.out.println(n + " is not prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    System.out.println(n + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(n + " is prime number");
+            }
+        } //end of else   
+    }
+}
