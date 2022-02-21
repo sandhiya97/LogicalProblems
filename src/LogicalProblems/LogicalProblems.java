7c399852f2f8a27bff835e08bc852e7aeeb6430f
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-21 07:29:53 +0530

    Fibonacci Series Program using Java

--- src/LogicalProblems/LogicalProblems.java
+++ src/LogicalProblems/LogicalProblems.java
@@ -2,6 +2,16 @@ package LogicalProblems;
 
 public class LogicalProblems {
 	 public static void main(String[] args) {
-         System.out.println("Welcome to Logical Problems");
+         System.out.println("Fibonacci Series");
+         int n1=0,n2=1,n3,i,count=10;    
+         System.out.print(n1+" "+n2);//printing 0 and 1    
+            
+         for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+         {    
+          n3=n1+n2;    
+          System.out.print(" "+n3);    
+          n1=n2;    
+          n2=n3;    
+         }    
      }
 }
